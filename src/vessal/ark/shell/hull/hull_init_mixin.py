ad708ac73626cbf9c6f9386d3d95b97f4ab68ec4
zale <zaleli@proton.me>
2026-04-21 18:16:13 +0800

    refactor(cell): compaction K/N defaults live only in Kernel constructor

--- src/vessal/ark/shell/hull/hull_init_mixin.py
+++ src/vessal/ark/shell/hull/hull_init_mixin.py
@@ -116,8 +116,10 @@ class HullInitMixin:
         self._cell.set("_data_dir", str(self._project_dir / "data"))
         compress_threshold = hull_cfg.get("compress_threshold", 50)
         self._cell.set("_compress_threshold", compress_threshold)
-        self._cell.set("_compaction_k", hull_cfg.get("compaction_k", 16))
-        self._cell.set("_compaction_n", hull_cfg.get("compaction_n", 8))
+        if "compaction_k" in hull_cfg:
+            self._cell.set("_compaction_k", hull_cfg["compaction_k"])
+        if "compaction_n" in hull_cfg:
+            self._cell.set("_compaction_n", hull_cfg["compaction_n"])
 
         self._routes: dict[tuple[str, str], object] = {}
         self._running_servers: dict[str, object] = {}
